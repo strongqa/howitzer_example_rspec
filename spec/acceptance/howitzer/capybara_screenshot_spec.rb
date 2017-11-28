168eb6e96ea956e9120dd73d2130934241d3c0bb
lecik <lec1k2103@gmail.com>
2017-11-28 19:56:54 +0200

    feature changes

--- spec/acceptance/howitzer/capybara_screenshot_spec.rb
+++ spec/acceptance/howitzer/capybara_screenshot_spec.rb
@@ -7,6 +7,8 @@ feature 'Howitzer - Capybara screenshot integration' do
       Capybara::Screenshot.screenshot_and_save_page
     end
     screenshot = File.join(File.dirname(__FILE__), '../../../log/screenshot.png')
-    expect(File.exist?(screenshot)).to be_truthy
+    expect(File).to be_exist(screenshot)
+    source_page = File.join(File.dirname(__FILE__), '../../../log/screenshot.html')
+    expect(File).to be_exist(source_page)
   end
 end
