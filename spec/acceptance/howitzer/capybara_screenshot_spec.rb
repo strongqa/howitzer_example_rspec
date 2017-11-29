13efa6a2a2b1b8e22c15fde71a2f3b8998317239
Alexey Voronenko <alexey.voronenko96@gmail.com>
2017-11-29 13:09:37 +0200

    Screenshot feature (#15)
    * screenshot feature added
    
    * CI fix
    
    * feature correction
    
    * config updated
    
    * another config update
    
    * feature changes

--- spec/acceptance/howitzer/capybara_screenshot_spec.rb
+++ spec/acceptance/howitzer/capybara_screenshot_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 RSpec.feature 'Howitzer - Capybara screenshot integration' do
-  scenario 'Test suite can create page screenshot' do
+  scenario 'Capybara screenshot library stores screenshot and source page in proper location' do
     HomePage.open
     HomePage.on do
       Capybara::Screenshot.screenshot_and_save_page
     end
     screenshot = File.join(File.dirname(__FILE__), '../../../log/screenshot.png')
-    expect(File.exist?(screenshot)).to be_truthy
+    expect(File).to be_exist(screenshot)
+    source_page = File.join(File.dirname(__FILE__), '../../../log/screenshot.html')
+    expect(File).to be_exist(source_page)
   end
 end
