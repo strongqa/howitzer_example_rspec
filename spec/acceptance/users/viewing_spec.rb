60532a3b14852f01713de757d708393153810ef5
Maxim <maximkalavrenenko@gmail.com>
2018-06-08 11:14:56 +0300

    minor

--- spec/acceptance/users/viewing_spec.rb
+++ spec/acceptance/users/viewing_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 RSpec.feature 'Viewing Users By Users with different roles' do
-  scenario "Non-admin user can't be viewing other user on user page" do
+  scenario "Non-admin user can't be viewing other user on users page" do
     user2 = create(:user)
     log_in_as(user2)
     UsersPage.open(validate: false)
     expect(HomePage).to be_displayed
   end
 
-  scenario 'Admin user can be viewing other user on user page' do
+  scenario 'Admin user can be viewing other users on user page' do
     user1 = create(:user, :admin)
     user2 = create(:user)
     log_in_as(user1)
