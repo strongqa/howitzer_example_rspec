502d8c753458514b1c22addbae9a694ba036770a
Vitalii Franchuk <fravit4@gmail.com>
2019-09-20 12:59:53 +0300

    rerun checks

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -ev
-if [[ "$SEXY_SETTINGS" =~ .*headless_chrome.* ]]
-then
-    wget https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
-    unzip chromedriver_linux64.zip
-    sudo cp chromedriver /usr/local/bin/chromedriver
-    sleep 3
-fi
 if [[ "$SEXY_SETTINGS" =~ .*headless_firefox.* ]]
 then
     wget https://github.com/mozilla/geckodriver/releases/download/v0.25.0/geckodriver-v0.25.0-linux64.tar.gz
     mkdir geckodriver
     tar -xzf geckodriver-v0.25.0-linux64.tar.gz -C geckodriver
     export PATH=$PATH:$PWD/geckodriver
+    sleep 3
+fi
+if [[ "$SEXY_SETTINGS" =~ .*headless_chrome.* ]]
+then
+    wget https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
+    unzip chromedriver_linux64.zip
+    sudo cp chromedriver /usr/local/bin/chromedriver
     sleep 3
 fi
 if [[ "$SEXY_SETTINGS" =~ .*webkit.* ]]
