6e66f68c639630414dab16472c52aa42c52027ab
Yevhen Plakhotnikov <yevhen@strongs-Mac-mini.local>
2019-10-11 12:19:40 +0300

    Updated validation for tests

--- spec/acceptance/categories/categories_maintaining_spec.rb
+++ spec/acceptance/categories/categories_maintaining_spec.rb
@@ -21,8 +21,7 @@ RSpec.feature 'Category maintaining' do
         Capybara.current_session.accept_alert
       end
     end
-    sleep 2
-    CategoriesListPage.on { is_expected.not_to have_category_item_element(category.name, wait: 3) }
+    CategoriesListPage.on { is_expected.to have_no_category_item_element(category.name, wait: 3) }
   end
 
   scenario 'admin can edit existing category' do
@@ -40,8 +39,7 @@ RSpec.feature 'Category maintaining' do
         Capybara.current_session.accept_alert
       end
     end
-    sleep 2
-    CategoriesListPage.on { is_expected.not_to have_category_item_element(category_new, wait: 3) }
+    CategoriesListPage.on { is_expected.to have_no_category_item_element(category_new, wait: 3) }
   end
 
   scenario 'admin can delete existing category' do
@@ -55,7 +53,6 @@ RSpec.feature 'Category maintaining' do
         Capybara.current_session.accept_alert
       end
     end
-    sleep 2
-    CategoriesListPage.on { is_expected.not_to have_category_item_element(category1.name, wait: 3) }
+    CategoriesListPage.on { is_expected.to have_no_category_item_element(category1.name, wait: 3) }
   end
 end
