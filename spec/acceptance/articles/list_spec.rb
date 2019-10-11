efec280690ab417074761f0dfbce776b6ab51d38
Yevhen Plakhotnikov <yevhen@strongs-Mac-mini.local>
2019-10-11 17:13:54 +0300

    reverted changes

--- spec/acceptance/articles/list_spec.rb
+++ spec/acceptance/articles/list_spec.rb
@@ -9,10 +9,13 @@ RSpec.feature 'Articles list' do
 
   scenario 'User view articles list', smoke: true do
     article1 = @article1
+    article2 = @article2
     ArticleListPage.open
     ArticleListPage.on do
       expect(text).to include(article1.title.upcase)
       expect(text).to include(article1.text)
+      expect(text).to include(article2.title.upcase)
+      expect(text).to include(article2.text)
     end
   end
 
