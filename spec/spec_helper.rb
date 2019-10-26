473962437bbf4155c0f746ed33f36f18cb7d63d3
Vitalii Franchuk <fravit4@gmail.com>
2019-10-26 11:15:06 +0300

    add tests order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ RSpec.configure do |config|
 
   config.disable_monkey_patching!
   config.color = true
+  config.order = Howitzer.test_order.presence || :defined
 
   config.before(:each) do
     scenario_name =
