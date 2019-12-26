aadbce272fb1f00e5c9d052f4bb36a8d316288f5
Vitalii Franchuk <fravit4@gmail.com>
2019-12-26 17:13:58 +0200

    revert trigger

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 set -ev
-CUCUMBER_STATE="started"
-TURNIP_STATE="started"
-while [[ " ${CUCUMBER_STATE[*]} " == *"started"* ]] || [[ " ${TURNIP_STATE[*]} " == *"started"* ]]; do
-        sleep 5
-        CUCUMBER_STATE=$(curl -s -X GET \
-       -H "Content-Type: application/json" \
-       -H "Accept: application/json" \
-       -H "Travis-API-Version: 3" \
-       -H "Authorization: token ${QA_TOKEN}" \
-       https://api.travis-ci.org/repo/${CUCUMBER_SLUG}/builds | grep -Po '"state":.*?[^\\]",'| awk -F "\"" '{print $4}' )
-        TURNIP_STATE=$(curl -s -X GET \
-       -H "Content-Type: application/json" \
-       -H "Accept: application/json" \
-       -H "Travis-API-Version: 3" \
-       -H "Authorization: token ${QA_TOKEN}" \
-       https://api.travis-ci.org/repo/${TURNIP_STATE}/builds | grep -Po '"state":.*?[^\\]",'| awk -F "\"" '{print $4}' )
-done
 if [[ "$SEXY_SETTINGS" =~ .*headless_firefox.* ]]
 then
     wget https://github.com/mozilla/geckodriver/releases/download/v0.25.0/geckodriver-v0.25.0-linux64.tar.gz
